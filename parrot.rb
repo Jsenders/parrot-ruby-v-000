dd6ec818586c306190cbafc8232fc95214d6ae00
Joseph Senders <jasenders@gmail.com>
2018-06-05 21:35:46 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(argument)
-  puts "Pretty bird!"
-  return "Squawk!"
+def parrot
+  puts "Squawk!"
+  "Squawk!"
 end
+
+
